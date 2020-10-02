272527b7057624155de0db686ab226999ce28c40
andreaives <andreasue3@gmail.com>
2020-10-01 20:26:42 -0600

    Final code

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 
 for (i=0; i<hour.length; i++) {
  var createDiv = $(`<div id="hour-${hour[i]}" class="row time-block" value=${hour[i]}>`);
-  var currTextArea = $("<textarea>");
+  var createTextArea = $("<textarea>");
   $(".container").append(createDiv);
   var currTime = displayHour[i];
-  createDiv.append($("<div class= 'col-md-1 hour'>").text(currTime))
+  createDiv.append($("<div class= 'col-md-1 hour'>").text(currTime));
   createDiv.append(createTextArea.attr("class", "col-md-10 description").attr("id", "memo-area"));
   createDiv.append($("<button class='col-md-1 saveBtn button'>").text("save"));
   if (moment( ).format("H") == hour[i]) {
@@ -27,3 +27,13 @@ for (i=0; i<hour.length; i++) {
     
     localStorage.setItem(key, value);
    })
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"))
+$("#hour-10 .description").val(localStorage.getItem("hour-10"))
+$("#hour-11 .description").val(localStorage.getItem("hour-11"))
+$("#hour-12 .description").val(localStorage.getItem("hour-12"))
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
